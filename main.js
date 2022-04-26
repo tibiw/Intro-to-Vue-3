1ce7732c6356ecd7098e8cb9a16886223be94e2d
tibiw <tibor.waxmann@gmail.com>
2022-04-26 23:52:11 +0300

    Lesson 8

--- main.js
+++ main.js
@@ -3,12 +3,22 @@ const app = Vue.createApp({
     return {
       cart: 0,
       product: "Socks",
-      image: "./assets/images/socks_green.jpg",
-      inventory: 0,
+      brand: "Vue Mastery",
+      selectedVariant: 0,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
-        { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
-        { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
+        {
+          id: 2234,
+          color: "green",
+          image: "./assets/images/socks_green.jpg",
+          quantity: 50,
+        },
+        {
+          id: 2235,
+          color: "blue",
+          image: "./assets/images/socks_blue.jpg",
+          quantity: 0,
+        },
       ],
     };
   },
@@ -18,8 +28,22 @@ const app = Vue.createApp({
       this.cart += 1;
     },
 
-    updateImage(variantImage) {
-      this.image = variantImage;
+    updateVariant(index) {
+      this.selectedVariant = index;
+    },
+  },
+
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+
+    inStock() {
+      return this.variants[this.selectedVariant].quantity;
     },
   },
 });
