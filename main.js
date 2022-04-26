bba246a9e4da59abcb4c58d519f03210b7da6add
tibiw <tibor.waxmann@gmail.com>
2022-04-26 23:33:06 +0300

    Lesson 7

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const app = Vue.createApp({
       cart: 0,
       product: "Socks",
       image: "./assets/images/socks_green.jpg",
-      inventory: 10,
+      inventory: 0,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
         { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
